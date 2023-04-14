978de278e8829fc2e4bd67aa4beff5cf2eb28ba3
Derek Honeycutt <derekshoneycutt@gmail.com>
2023-04-13 19:57:06 -0700

    Update Point Highlighting to support 3D

--- ElementsBook1/AddedAxioms/src/001-PointHighlight.jl
+++ ElementsBook1/AddedAxioms/src/001-PointHighlight.jl
@@ -1,17 +1,20 @@
 
-export EuclidPoint2fHighlight, highlight, show_complete, hide, animate
+export EuclidPointHighlight, EuclidPoint2fHighlight, EuclidPoint3fHighlight, highlight, show_complete, hide, animate
 
 """
-    EuclidPoint2fHighlight
+    EuclidPointHighlight
 
 Describes highlighting a point in a Euclid diagram
 """
-mutable struct EuclidPoint2fHighlight
-    baseOn::EuclidPoint2f
+mutable struct EuclidPointHighlight{N}
+    baseOn::EuclidPoint{N}
     plots
     current_width::Observable{Float32}
     max_width::Observable{Float32}
 end
+
+EuclidPoint2fHighlight = EuclidPointHighlight{2}
+EuclidPoint3fHighlight = EuclidPointHighlight{3}
 
 """
 highlight(point[, width=0.02f0, color=:red])
@@ -29,8 +32,28 @@ function highlight(point::EuclidPoint2f;
     observable_highlight = Observable(0f0)
     observable_max_width = width isa Observable{Float32} ? width : Observable(width)
 
-    use_color = get_color(color)
-    plots = poly!(@lift(Circle($(point.data), $observable_highlight)), color=RGBA(use_color.r, use_color.g, use_color.b, 0.6))
+    plots = poly!(@lift(Circle($(point.data), $observable_highlight)), color=opacify(color, 0.6))
+
+    EuclidPoint2fHighlight(point, plots, observable_highlight, observable_max_width)
+end
+
+"""
+highlight(point[, width=0.02f0, color=:red])
+
+Set up highlighting a single point in a Euclid diagram
+
+# Arguments
+- `point::EuclidPoint3f`: The point to highlight in the diagram
+- `width::Union{Float32, Observable{Float32}}`: The width of the circle to draw the highlight
+- `color`: The color to use in highlighting the point
+"""
+function highlight(point::EuclidPoint3f;
+                   width::Union{Float32, Observable{Float32}}=0.02f0, color=:red)
+
+    observable_highlight = Observable(0f0)
+    observable_max_width = width isa Observable{Float32} ? width : Observable(width)
+
+    plots = mesh!(@lift(Sphere($(point.data), $observable_highlight)), color=opacify(color, 0.6))
 
     EuclidPoint2fHighlight(point, plots, observable_highlight, observable_max_width)
 end
@@ -41,9 +64,9 @@ end
 Complete a previously defined highlight operation for a point in a Euclid diagram. It will be fully highlighted.
 
 # Arguments
-- `point::EuclidPoint2fHighlight`: The description of the highlight to finish
+- `point::EuclidPointHighlight`: The description of the highlight to finish
 """
-function show_complete(point::EuclidPoint2fHighlight)
+function show_complete(point::EuclidPointHighlight)
     point.current_width[] = point.max_width
 end
 
@@ -53,9 +76,9 @@ end
 Hide highlights of a point in a Euclid diagram
 
 # Arguments
-- `point::EuclidPoint2fHighlight`: The description of the highlight to completely hide
+- `point::EuclidPointHighlight`: The description of the highlight to completely hide
 """
-function hide(point::EuclidPoint2fHighlight)
+function hide(point::EuclidPointHighlight)
     point.current_width[] = 0f0
 end
 
@@ -65,14 +88,14 @@ end
 Animate highlighting a point in a Euclid diagram
 
 # Arguments
-- `point::EuclidPoint2fHighlight`: The point to animate in the diagram
+- `point::EuclidPointHighlight`: The point to animate in the diagram
 - `hide_until::AbstractFloat`: The time point to begin highlighting the point at
 - `max_at::AbstractFloat`: The time point to have maximum highlight at
 - `min_at::AbstractFloat`: The time point to finish going back to no more highlighting
 - `t::AbstractFloat`: The current timeframe of the animation
 """
 function animate(
-    point::EuclidPoint2fHighlight, hide_until::AbstractFloat, max_at::AbstractFloat, min_at::AbstractFloat, t::AbstractFloat)
+    point::EuclidPointHighlight, hide_until::AbstractFloat, max_at::AbstractFloat, min_at::AbstractFloat, t::AbstractFloat)
 
     perform(t, hide_until, max_at, max_at + 0.00001, min_at,
         () -> point.current_width[] = 0f0,
