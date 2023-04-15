019bcc5e700bd25252c49b121085e627aaba69d9
Derek Honeycutt <derekshoneycutt@gmail.com>
2023-04-14 19:27:40 -0700

    Line Extremities add 3D

--- ElementsBook1/Definitions/src/003-LineExtremities.jl
+++ ElementsBook1/Definitions/src/003-LineExtremities.jl
@@ -1,18 +1,20 @@
 
-export EuclidLine2fHighlightExtremities, highlight_extremities, show_complete, hide, animate
+export EuclidLineHighlightExtremities, EuclidLine2fHighlightExtremities, EuclidLine3fHighlightExtremities, highlight_extremities, show_complete, hide, animate
 
 """
-    EuclidLine2fHighlightExtremities
+    EuclidLineHighlightExtremities{N}
 
 Describes highlighting a line's extremities in a Euclid diagram
 """
-mutable struct EuclidLine2fHighlightExtremities
-    baseOn::EuclidLine2f
-    extremity_A::EuclidPoint2f
-    highlight_A::EuclidPoint2fHighlight
-    extremity_B::EuclidPoint2f
-    highlight_B::EuclidPoint2fHighlight
+mutable struct EuclidLineHighlightExtremities{N}
+    baseOn::EuclidLine{N}
+    extremity_A::EuclidPoint{N}
+    highlight_A::EuclidPointHighlight{N}
+    extremity_B::EuclidPoint{N}
+    highlight_B::EuclidPointHighlight{N}
 end
+EuclidLine2fHighlightExtremities = EuclidLineHighlightExtremities{2}
+EuclidLine3fHighlightExtremities = EuclidLineHighlightExtremities{3}
 
 """
     highlight_extremities(line[, point_width=0.02f0, point_color=:blue, text_color=:blue, text_opacity=1f0, labelA="A", labelB="B"])
@@ -45,6 +47,23 @@ function highlight_extremities(line::EuclidLine2f;
 
     EuclidLine2fHighlightExtremities(line, extremity_A, highlight_A, extremity_B, highlight_B)
 end
+function highlight_extremities(line::EuclidLine3f;
+    point_width::Union{Float32, Observable{Float32}}=0.04f0, point_color=:blue,
+    text_color=:blue, text_opacity::Union{Float32, Observable{Float32}}=1f0, labelA="A", labelB="B")
+
+    extremity_A = point(line.extremityA,
+                        point_width=point_width*0.001f0, point_color=point_color,
+                        text_color=text_color, text_opacity=text_opacity,
+                        label=labelA)
+    highlight_A = highlight(extremity_A, width=point_width, color=point_color)
+    extremity_B = point(line.extremityB,
+                        point_width=point_width*0.001f0, point_color=point_color,
+                        text_color=text_color, text_opacity=text_opacity,
+                        label=labelB)
+    highlight_B = highlight(extremity_B, width=point_width, color=point_color)
+
+    EuclidLine3fHighlightExtremities(line, extremity_A, highlight_A, extremity_B, highlight_B)
+end
 
 """
     show_complete(line)
@@ -52,9 +71,9 @@ end
 Complete a previously defined highlight operation for a line's extremities in a Euclid diagram. It will be fully highlighted.
 
 # Arguments
-- `line::EuclidLine2fHighlightExtremities`: The description of the highlight to finish
+- `line::EuclidLineHighlightExtremities`: The description of the highlight to finish
 """
-function show_complete(line::EuclidLine2fHighlightExtremities)
+function show_complete(line::EuclidLineHighlightExtremities)
     show_complete(line.extremity_A)
     show_complete(line.highlight_A)
     show_complete(line.extremity_B)
@@ -67,9 +86,9 @@ end
 Hide highlights of a line's extremities in a Euclid diagram
 
 # Arguments
-- `line::EuclidLine2fHighlightExtremities`: The description of the highlight to completely hide
+- `line::EuclidLineHighlightExtremities`: The description of the highlight to completely hide
 """
-function hide(line::EuclidLine2fHighlightExtremities)
+function hide(line::EuclidLineHighlightExtremities)
     hide(line.extremity_A)
     hide(line.highlight_A)
     hide(line.extremity_B)
@@ -82,14 +101,14 @@ end
 Animate highlighting a line's extremities in a Euclid diagram
 
 # Arguments
-- `line::EuclidLine2fHighlightExtremities`: The line to animate in the diagram
+- `line::EuclidLineHighlightExtremities`: The line to animate in the diagram
 - `hide_until::AbstractFloat`: The time point to begin highlighting the line at
 - `max_at::AbstractFloat`: The time point to have maximum highlight at
 - `min_at::AbstractFloat`: The time point to finish going back to no more highlighting
 - `t::AbstractFloat`: The current timeframe of the animation
 """
 function animate(
-    line::EuclidLine2fHighlightExtremities, hide_until::AbstractFloat, max_at::AbstractFloat, min_at::AbstractFloat, t::AbstractFloat)
+    line::EuclidLineHighlightExtremities, hide_until::AbstractFloat, max_at::AbstractFloat, min_at::AbstractFloat, t::AbstractFloat)
 
     max_time = max_at - hide_until
     animate(line.extremity_A, hide_until, hide_until + max_time/2f0, t, fade_start=max_at+0.0001, fade_end=min_at)
