80f822539d83bd61c83b28b85855f51c72dc2e4c
Derek Honeycutt <derekshoneycutt@gmail.com>
2023-04-19 14:24:40 -0700

    Add 3D support for surfaces

--- ElementsBook1/Definitions/src/005-Surface.jl
+++ ElementsBook1/Definitions/src/005-Surface.jl
@@ -1,22 +1,24 @@
 
-export EuclidSurface2f, just_surface, show_complete, hide, animate
+export EuclidSurface, EuclidSurface2f, EuclidSurface3f, plane_surface, show_complete, hide, animate
 
 """
-    EuclidSurface2f
+    EuclidSurface
 
 Describes a surface to be drawn and animated in Euclid diagrams
 """
-mutable struct EuclidSurface2f
-    from_points::Observable{Vector{Point2f}}
+mutable struct EuclidSurface{N}
+    from_points::Observable{Vector{Point{N, Float32}}}
     plots
     current_opacity::Observable{Float32}
     show_opacity::Observable{Float32}
 end
+EuclidSurface2f = EuclidSurface{2}
+EuclidSurface3f = EuclidSurface{3}
 
 
 
 """
-    just_surface(points[, color=:blue, opacity=1f0])
+plane_surface(points[, color=:blue, opacity=1f0])
 
 Sets up a new surface in a Euclid Diagram for drawing, only drawing the surface itself
 
@@ -25,7 +27,7 @@ Sets up a new surface in a Euclid Diagram for drawing, only drawing the surface 
 - `color`: The color to draw the line with
 - `opacity::Union{Float32, Observable{Float32}}`: The opacity to show the surface at
 """
-function just_surface(points::Observable{Vector{Point2f}};
+function plane_surface(points::Observable{Vector{Point2f}};
                       color=:blue, opacity::Union{Float32,Observable{Float32}}=1f0)
 
     observable_opacity = Observable(0f0)
@@ -40,22 +42,30 @@ function just_surface(points::Observable{Vector{Point2f}};
     EuclidSurface2f(points, plots,
                     observable_opacity, observable_show_opacity)
 end
-
-
-"""
-    just_surface(points[, color=:blue, opacity=1f0])
-
-Sets up a new surface in a Euclid Diagram for drawing, only drawing the surface itself
-
-# Arguments
-- `points::Vector{Point2f}`: The points to draw the surface within
-- `color`: The color to draw the line with
-- `opacity::Union{Float32, Observable{Float32}}`: The opacity to show the surface at
-"""
-function just_surface(points::Vector{Point2f};
+function plane_surface(points::Vector{Point2f};
                       color=:blue, opacity::Union{Float32,Observable{Float32}}=1f0)
 
-    just_surface(Observable(points),
+    plane_surface(Observable(points),
+                 color=color, opacity=opacity)
+end
+function plane_surface(points::Observable{Vector{Point3f}};
+                      color=:blue, opacity::Union{Float32,Observable{Float32}}=1f0)
+
+    observable_opacity = Observable(0f0)
+    observable_show_opacity = opacity isa Observable{Float32} ?
+                              opacity :
+                              Observable(opacity)
+
+    plots = mesh!(points, color=@lift(opacify(color, $observable_opacity)),
+                  strokewidth=0f0)
+
+    EuclidSurface3f(points, plots,
+                    observable_opacity, observable_show_opacity)
+end
+function plane_surface(points::Vector{Point3f};
+                      color=:blue, opacity::Union{Float32,Observable{Float32}}=1f0)
+
+    plane_surface(Observable(points),
                  color=color, opacity=opacity)
 end
 
@@ -66,9 +76,9 @@ end
 Completely show previously defined surface in a Euclid diagram
 
 # Arguments
-- `surface::EuclidSurface2f`: The surface to completely show
+- `surface::EuclidSurface`: The surface to completely show
 """
-function show_complete(surface::EuclidSurface2f)
+function show_complete(surface::EuclidSurface)
     surface.current_opacity[] = surface.show_opacity[]
 end
 
@@ -78,9 +88,9 @@ end
 Completely hide previously defined surface in a Euclid diagram
 
 # Arguments
-- `surface::EuclidSurface2f`: The surface to completely hide
+- `surface::EuclidSurface`: The surface to completely hide
 """
-function hide(surface::EuclidSurface2f)
+function hide(surface::EuclidSurface)
     surface.current_opacity[] = 0f0
 end
 
@@ -90,7 +100,7 @@ end
 Animate drawing and perhaps then hiding surface drawn in a Euclid diagram
 
 # Arguments
-- `surface::EuclidSurface2f`: The surface to animate in the diagram
+- `surface::EuclidSurface`: The surface to animate in the diagram
 - `hide_until::AbstractFloat`: The point to hide the surface until
 - `max_at::AbstractFloat`: The time to max drawing the surface at -- when it is fully drawn
 - `t::AbstractFloat`: The current timeframe of the animation
@@ -98,7 +108,7 @@ Animate drawing and perhaps then hiding surface drawn in a Euclid diagram
 - `fade_end::AbstractFloat`: When to end fading the surface awawy from the diagram -- it will be hidden entirely
 """
 function animate(
-    surface::EuclidSurface2f, hide_until::AbstractFloat, max_at::AbstractFloat, t::AbstractFloat;
+    surface::EuclidSurface, hide_until::AbstractFloat, max_at::AbstractFloat, t::AbstractFloat;
     fade_start::AbstractFloat=0f0, fade_end::AbstractFloat=0f0)
 
     perform(t, hide_until, max_at, fade_start, fade_end,
